c2a910045bdd0fdde8c3cad2b4d7dadea951a50c
Patrick <patrick.hurley@codeimmersives.com>
2020-02-18 16:18:19 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-function crazyCase() {
+function crazyCase(str) {
+  let answer = '';
+  for (let i = 0; i < str.length; i++){
+  if(i % 2 !== 0){
+  answer = answer + str[i].toUpperCase();
+} else {
+  answer = answer + str[i].toLowerCase();
 }
-
+}
+return answer;
+}
 function ciEmailify() {
 }
 
